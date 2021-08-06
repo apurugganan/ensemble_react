1a1495b9555533019653a7f1af70ce2d587b6bf1
Aj Purugganan <apurugganan1@bcit.ca>
2021-08-06 16:00:42 -0700

    Styling part 1

--- src/App.js
+++ src/App.js
@@ -14,21 +14,30 @@ function App() {
 
   return (
     <div className="App">
+      <div className="container">
+
         {
           students.map( student => { return (
-            <div>
-              <img src={student.pic} alt="student"/>
-              <p>{student.firstName} {student.lastName}</p>
-              <p>{student.email}</p>
-              <p>{student.company}</p>
-              <p>{student.skill}</p>
-              <p>{student.grades.reduce((acc, x) => (
-                  parseFloat(acc) + parseFloat(x))) / student.grades.length
-                }
-              </p>
+            <div className="student-card">
+              <div className="student-card-avatar">
+                <img className="student-card-avatar-img" src={student.pic} alt="student"/>
+              </div>  
+              <div className="student-text">
+                <p className="student-name">{student.firstName} {student.lastName}</p>
+                <div className="student-details">
+                  <p>{student.email}</p>
+                  <p>{student.company}</p>
+                  <p>{student.skill}</p>
+                  <p>{student.grades.reduce((acc, x) => (
+                    parseFloat(acc) + parseFloat(x))) / student.grades.length
+                  }
+                  </p>
+                </div>
+              </div>
             </div>
           )})
         }
+      </div>
     </div>
   );
 }
