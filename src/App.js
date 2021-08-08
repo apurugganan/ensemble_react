8375d67ac92a06eec856df40fc317a59a7a44f9e
apurugganan <apurugganan1@my.bcit.ca>
2021-08-08 11:16:58 -0700

    CSS Styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {useEffect, useState} from 'react';
 function App() {
   const [students, setStudents] = useState([]);
 
+  // Get data from API
   useEffect(()=>{
     (async ()=> {
       const result = await getData();
@@ -15,7 +16,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-
         {
           students.map( student => { return (
             <div className="student-card">
@@ -23,15 +23,15 @@ function App() {
                 <img className="student-card-avatar-img" src={student.pic} alt="student"/>
               </div>  
               <div className="student-text">
-                <p className="student-name">{student.firstName} {student.lastName}</p>
+                <h2 className="student-name">{student.firstName.toUpperCase()} {student.lastName.toUpperCase()}</h2>
                 <div className="student-details">
-                  <p>{student.email}</p>
-                  <p>{student.company}</p>
-                  <p>{student.skill}</p>
-                  <p>{student.grades.reduce((acc, x) => (
+                  <p>Email: {student.email}</p>
+                  <p>Company: {student.company}</p>
+                  <p>Skill: {student.skill}</p>
+                  <p>Average: {student.grades.reduce((acc, x) => (
                     parseFloat(acc) + parseFloat(x))) / student.grades.length
                   }
-                  </p>
+                   %</p>
                 </div>
               </div>
             </div>
@@ -53,3 +53,5 @@ async function getData(){
     console.log(error);
   }
 }
+
+
