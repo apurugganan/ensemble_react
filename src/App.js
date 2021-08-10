53838d98dd2aa3c10c524835e7a96908ed9c7edb
apurugganan <apurugganan1@my.bcit.ca>
2021-08-09 21:39:06 -0700

    Double Filter Search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Card from './Card'
 function App() {
   const [students, setStudents] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchTag, setSearchTag] = useState("");
 
   function addTag(args, index){ 
     if(!students[index].tags){
@@ -21,7 +22,11 @@ function App() {
   useEffect(()=>{
     (async ()=> {
       const result = await getData();
-      setStudents(result.students);
+      let newArray = result.students
+      for( let i = 0; i< newArray.length; i++){
+        newArray[i]['tags'] = [];
+      }
+      setStudents([...newArray])
     })()
   },[]);
 
@@ -35,24 +40,40 @@ function App() {
             onChange={event =>{setSearchTerm(event.target.value)} }
             />
         </div>
+        <div className="search-tag">
+          <input className="search-tag-input"
+            type="text" 
+            placeholder="Search by Tag"
+            onChange={event =>{setSearchTag(event.target.value)} }
+            />
+        </div>
         {
           // Search by Name feature
-          students.filter(student => {
-            if(searchTerm === ""){
-              return student
-            } else if (
-                student.firstName.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-                || student.lastName.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-              ){
-             
-              return student;
-            } else {
-              return null;
-            }
+          students
+            .filter(student => {
+              if(searchTerm === ""){
+                return student
+              } else if (
+                  student.firstName.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+                  || student.lastName.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+                ){
+              
+                return student;
+              } else {
+                return null;
+              }
+            })
+            .filter(student => {
+              if(searchTag === ""){
+                return student
+              } else if (student.tags.includes(searchTag)){
+                return student
+              } else {
+                return null;
+              } 
             })
             // Display List of Student Cards 
             .map((student, index) => {  
-            
               return (
               // Student Card 
               <Card 
